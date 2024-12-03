50c4ad5ff27835514069e8617f54b88417b9e556
Sebastian Messmer <messmer@cryfs.org>
2024-12-03 15:50:46 -0800

    Comment out unused code

--- plaid/src/plaid_api/mod.rs
+++ plaid/src/plaid_api/mod.rs
@@ -6,7 +6,7 @@ mod test_connection;
 mod transactions;
 
 pub use accounts::get_accounts;
-pub use categories::lookup_category;
+// pub use categories::lookup_category;
 pub use client::Plaid;
 pub use link_account::link_new_account;
 pub use test_connection::test_connection;
